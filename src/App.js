c368179a5f56cd09f2b065106b5d5beefe75fe83
antoniusbayu76 <antoniusbayu96@gmail.com>
2024-07-09 18:17:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className='nama-tim'>Nama Tim: SAFINAH-ONE PT: GAMANTARAY</div>
+        <div className='lintasan'>Lintasan: .... ( A / B )</div>
+      </div>
+      <div className="content">
+        <div className="position-log">
+          <div className='log-tittle'>Position-Log</div>
+          <ol>
+            <li>Preparation</li>
+            <li>Start</li>
+            <li>Floating ball set 1-10</li>
+            <li>Mission Surface Imaging </li>
+            <li>Mission Underwater Imaging </li>
+            <li>Finish</li>
+          </ol>
+        </div>
+        <div className="scores">
+          <div className="score">4</div>
+          <div className="score">5</div>
+        </div>
+      </div>
+      <div className="attitudes">
+        <div className="attitudeinfo">
+          <div className='attitude-tittle'>Attitude Information</div>
+          <ul>
+            <li>Trajectory graph (mapping)</li>
+            <li>SOG</li>
+            <li>COG</li>
+          </ul>
+        </div>
+        <div className="attitude">Attitude</div>
+      </div>
+      <div className="other-indicators">
+        <div>Indikator Lain (bebas):</div>
+        <ul>
+          <li>Battery Level</li>
+          <li>Visual Video</li>
+        </ul>
+      </div>
     </div>
   );
 }
